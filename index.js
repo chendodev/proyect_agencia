06f080979072d31cb2b4180471e5915ff992e61f
chendo <chendoec@live.com>
2021-12-29 13:31:40 -0500

    Proyect Agencia

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import router from './routes/index.js'
+import db from './config/db.js';
+import dotenv from 'dotenv';
+dotenv.config({ path: 'variables.env' });
+
+const app = express();
+
+//Conectar la base de datos
+db.authenticate()
+    .then( () => console.log('Base de datos conectada') )
+    .catch( error => console.log(error));
+
+// const port = process.env.HTTP_PORT || 4000;
+
+// Habilitar PUG
+app.set('view engine', 'pug');
+
+// Obtener el año actual
+app.use( (req, res, next) => {
+    const year = new Date();
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombreSitio = 'Agencia de Viajes'
+    next();
+});
+
+//Agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+// Definir la carpeta pública
+app.use(express.static('public'));
+
+// Agregar Router
+app.use('/', router);
+
+// Puerto y host para la app
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000
+
+app.listen(port, host, () => {
+    console.log('El servidor esta funcionando');
+});
